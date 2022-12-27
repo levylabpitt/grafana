eda8ae4a931bc8263499701046ee059ace1439e5
Elias Benbourenane <eliasbenbourenane@gmail.com>
2022-12-27 12:48:06 -0500

    Implemented column name mapping in Grafana's 'Panel Editor'

--- public/app/features/plugins/sql/components/TableSelector.tsx
+++ public/app/features/plugins/sql/components/TableSelector.tsx
@@ -7,6 +7,14 @@ import { Select } from '@grafana/ui';
 import { QueryWithDefaults } from '../defaults';
 import { DB, ResourceSelectorProps } from '../types';
 
+interface TableIndex {
+  name: string
+  path: string
+}
+
+// currently requires hardcoded data. A POST request to /api/ds/query needs to be made for table_index in the db
+const table_index: TableIndex[] = [];
+
 interface TableSelectorProps extends ResourceSelectorProps {
   db: DB;
   value: string | null;
@@ -23,6 +31,20 @@ export const TableSelector: React.FC<TableSelectorProps> = ({ db, query, value, 
     const tables = await db.tables(query.dataset);
     return tables.map(toOption);
   }, [query.dataset]);
+
+
+  // filters anything that doesn't include 'llab' and also maps the label for each column with table_index
+  if (state.value) {
+    for (let i = 0; i < state.value.length; i++) {
+      if (!state.value[i].value.endsWith("_index") && state.value[i].value.startsWith("llab_")) {
+        var match = table_index.map(function (o) { return o.path; }).indexOf(state.value[i].value);
+        state.value[i].label = match == -1 ? state.value[i].label : table_index[match].name;
+      } else {
+        state.value.splice(i, 1);
+        i--;
+      }
+    }
+  }
 
   return (
     <Select
