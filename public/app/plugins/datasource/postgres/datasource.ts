29ca47bbcb2a3204a2cf8c3511f9ca46ad04429a
Elias Benbourenane <eliasbenbourenane@gmail.com>
2022-12-28 17:16:20 -0500

    Removed empty backets on columns without units

--- public/app/plugins/datasource/postgres/datasource.ts
+++ public/app/plugins/datasource/postgres/datasource.ts
@@ -66,7 +66,10 @@ export class PostgresDatasource extends SqlDatasource {
     if (query.llab == 1 && query.rawSql) {
       for (let i = 0; i < schema.length; i++) {
         const type = schema.fields.name.type;
-        result.push({ label: `${schema.fields.name.values.get(i)} (${schema.fields.units.values.get(i)})`, value: schema.fields.path.values.get(i), type, ...getFieldConfig(type) });
+        const name = schema.fields.name.values.get(i);
+        const units = schema.fields.units.values.get(i);
+        const path = schema.fields.path.values.get(i);
+        result.push({ label: `${name}${units ? ` (${units})` : ''}`, value: path, type, ...getFieldConfig(type) });
       }
     } else if (query.llab == 2 && query.rawSql) {
       for (let i = 0; i < schema.length; i++) {
